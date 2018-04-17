52dbb6cdf05ece073648550743b324a5a1096882
Victor Isachsen <victor.isachsen@gmail.com>
2018-04-17 13:04:57 +0200

    Added style

--- Index1.php
+++ Index1.php
@@ -1,3 +1,38 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Quantified self</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+  <div class="container-fluid">
+    <header>
+      <h1>Quantified self</h1>
+    </header>
+  </div>
+<body>
+
+  <!-- Navigation -->
+
+  <nav class="navbar navbar-expand-lg">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">  
+          <a id="navbar" class="nav-item nav-link active" href="#">Hjem <span class="sr-only">(current)</span></a>
+          <a id="navbar" class="nav-item nav-link" href="Side her">Features</a>
+          <a id="navbar" class="nav-item nav-link" href="Index1.php">Login</a>
+        </div>
+      </div>
+    </nav>
 <?php 
 session_start(); 
 include 'connect.php';
@@ -40,3 +75,8 @@ $mysqli->close();
     <!-- sættes ind i URL --> 
     <a href="?logout=1">Logout</a>
 </p>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
--- userinformation.php
+++ userinformation.php
@@ -1,7 +1,8 @@
 <?php
-$host_db = "localhost";
-$user_db = "root";
-$password_db = "root";
-$database_db = "Loginsystem_db";
-$table1_db = "Login";
+$host_db = 'localhost';
+$user_db = 'root';
+$password_db = 'root';
+$database_db = 'Loginsystem_db';
+$table1_db = 'Login';
+$port = 3306;
 ?>
